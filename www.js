e64a289b3a091a4903a0fabec799ac986bc14851
Kryslin Rass <kryrass@users.noreply.github.com>
2023-10-05 17:29:29 +0300

    Tund 5 okt

--- www.js
+++ www.js
@@ -2,6 +2,7 @@ const http = require("http");
 const url = require("url");
 const path = require("path");
 const fs = require("fs");
+const querystring = require("querystring")
 const datetimeValue = require("./datetime_et");
 const semester = require("./semesterprogress");
 
@@ -14,18 +15,48 @@ const pageFoot = '<hr></body></html>'
 http.createServer(function(req, res){
 	let currentURL = url.parse(req.url, true);
 	//console.log(currentURL);
-	if (currentURL.pathname === "/"){
-	res.writeHead(200, {"Content-type": "text/html"});
-	res.write(pageHead);
-	res.write(pageBanner);
-	res.write(pageBody);
-	res.write("<p>Lehe avamise aeg oli " + datetimeValue.timeOfDayET() + " ja kell on " + datetimeValue.timeNowET() + "</p><p>Kuupäev on " + datetimeValue.dateNowET() + "</p>");
-	res.write('\n\t<hr>\n\t<p><a href ="addname">Lisa oma nimi</a>!</p>');
-	res.write('<p><a href="semesterprogress">Semestri kulg</a></p>');
-	res.write('<p><a href="picture">Tore pilt.</a></p>');
-	res.write(pageFoot);
-	//console.log("keegi vaatab");
-	return res.end();
+	if(req.method === 'POST'){
+		collectRequestData(req, result => {
+            console.log(result);
+            		//kirjutame andmeid tekstifaili
+			fs.open('public/log.txt', 'a', (err, file)=>{
+				if(err){
+					throw err;
+				}
+				else {
+					fs.appendFile('public/log.txt', result.firstNameInput + ';', (err)=>{
+						if(err){
+							throw err;
+						}
+						else {
+							console.log('faili kirjutati!');
+						}
+					});
+				}
+				/* fs.close(file, (err)=>{
+					if(err){
+						throw err;
+					}
+				}); */
+			});
+			
+			res.end(result.firstNameInput);
+			//res.end('Tuligi POST!');
+		});
+	}
+
+	else if (currentURL.pathname === "/"){
+		res.writeHead(200, {"Content-type": "text/html"});
+		res.write(pageHead);
+		res.write(pageBanner);
+		res.write(pageBody);
+		res.write("<p>Lehe avamise aeg oli " + datetimeValue.timeOfDayET() + " ja kell on " + datetimeValue.timeNowET() + "</p><p>Kuupäev on " + datetimeValue.dateNowET() + "</p>");
+		res.write('\n\t<hr>\n\t<p><a href ="addname">Lisa oma nimi</a>!</p>');
+		res.write('<p><a href="semesterprogress">Semestri kulg</a></p>');
+		res.write('<p><a href="picture">Tore pilt.</a></p>');
+		res.write(pageFoot);
+		//console.log("keegi vaatab");
+		return res.end();
 }
 
 else if (currentURL.pathname == "/addname"){
@@ -34,7 +65,8 @@ else if (currentURL.pathname == "/addname"){
 	res.write(pageBanner);
 	res.write(pageBody);
 	res.write('\n\t<hr>\n\t<h2>Lisa palun oma nimi</h2>');
-	res.write('\n\t<p>Edaspidi lisame siia asju<p>');
+	res.write('\n\t<form method="POST">\n\t\t<label for="firstNameInput">Eesnimi: </label>\n\t\t<input type="text" name="firstNameInput" id="firstNameInput" placeholder="Sinu eesnimi ...">\n\t\t<br>\n\t\t<label for="lastNameInput">Perekonnanimi: </label>\n\t\t<input type="text" name="lastNameInput" id="lastNameInput" placeholder="Sinu perekonnanimi ...">\n\t\t<br>\n\t\t<input type="submit" name="nameSubmit" value="Salvesta">\n\t</form>');
+	res.write('\n\t <p><a href="/">Tagasi avalehele</a>!</p>');
 	res.write(pageFoot);
 }
 else if (currentURL.pathname==="/semesterprogress")
@@ -48,53 +80,100 @@ else if (currentURL.pathname==="/semesterprogress")
 		res.write(pageFoot);
 		return res.end();
 	}
-else if (currentURL.pathname==="/picture")
-	{
-		res.writeHead(200, {"Content-Type":"text/html"});
-		res.write(pageHead);
-		res.write(pageBanner);
-		res.write(pageBody);
-		res.write(tluPicture);
-		res.write(pageFoot);
-		return res.end();
+else if (currentURL.pathname === "/picture"){
+		//loeme kataloogist fotode nimekirja ja loosime Ã¼he pildi
+		let htmlOutput = '\n\t<p>Pilti ei saa nÃ¤idata!</p>';
+		let listOutput = '';
+		fs.readdir('public/tluphotos', (err, fileList)=>{
+			if(err) {
+				throw err;
+				tluPhotoPage(res, htmlOutput, listOutput);
+			}
+			else {
+				//console.log(fileList);
+				let photoNum = Math.floor(Math.random() * fileList.length);
+				htmlOutput = '\n\t<img src="' + fileList[photoNum] + '" alt="TLÃœ pilt">';
+				//console.log(htmlOutput);
+				listOutput = '\n\t<ul>';
+				for (fileName of fileList){
+					listOutput += '\n\t\t<li>' + fileName + '</li>';
+				}
+				listOutput += '\n\t</ul>';
+				//console.log(listOutput);
+				tluPhotoPage(res, htmlOutput, listOutput);
+			}
+		});
 	}
-else if (currentURL.pathname==="/tlu_42.jpg")
-	{
-		console.log("Tahan pilti!");
-		let filePath=path.join(__dirname, "public", "tluphotos/tlu_42.jpg");
-		fs.readFile(filePath, (err, data)=>
-		{
-			if(err)
-			{
+
+	else if (currentURL.pathname === "/banner.png"){
+		//console.log("Tahame pilti!");
+		let bannerPath = path.join(__dirname, "public", "banner");
+		//console.log(bannerPath + currentURL.pathname);
+		fs.readFile(bannerPath + currentURL.pathname, (err, data)=>{
+			if (err) {
 				throw err;
 			}
-			else
-			{
-				res.writeHead(200, {"Content-Type":"image/png"});
+			else {
+				//console.log("Tuli Ã¤ra!");
+				res.writeHead(200, {"Content-type": "image/png"});
 				res.end(data);
 			}
 		});
 	}
 
-else if (currentURL.pathname === "/banner.png"){
-	console.log("Tahame pilti!");
-	let bannerPath = path.join(__dirname, "public", "banner");
-	console.log(bannerPath)
-	fs.readFile(bannerPath + currentURL.pathname, (err, data)=>{
-		if (err) {
-			throw err;
-		}
-		else {
-			res.writeHead(200, {"Content-type": "image/png"});
-			res.end(data);
-		}
-	});
-}
-else {
-	
-	res.end("error 404");
-}
+	//else if (currentURL.pathname === "/tlu_42.jpg"){
+	else if (path.extname(currentURL.pathname) === ".jpg"){
+		console.log(path.extname(currentURL.pathname));
+		//let filePath = path.join(__dirname, "public", "tluphotos/tlu_42.jpg");
+		let filePath = path.join(__dirname, "public", "tluphotos");
+		fs.readFile(filePath + currentURL.pathname, (err, data)=>{
+			if(err){
+				throw err;
+			}
+			else {
+				res.writeHead(200, {"Content-Type": "image/jpeg"});
+				res.end(data);
+			}
+		});
+	} 
+
+	else {
+		res.end("ERROR 404");
+	}
 }).listen(5109);
+
+function tluPhotoPage(res, htmlOut, listOutput){
+	res.writeHead(200, {"Content-Type": "text/html"});
+	res.write(pageHead);
+	res.write(pageBanner);
+	res.write(pageBody);
+	res.write('\n\t<hr>');
+	res.write(htmlOut);
+	if(listOutput != ''){
+		res.write(listOutput);
+	}
+	res.write('\n\t<img src="tlu_42.jpg" alt="TLÜ foto">');
+	res.write('\n\t <p><a href="/">Tagasi avalehele</a>!</p>');
+	res.write(pageFoot);
+	return res.end();
+}
+function collectRequestData(request, callback) {
+    const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+    if(request.headers['content-type'] === FORM_URLENCODED) {
+        let receivedData = '';
+        request.on('data', chunk => {
+            receivedData += chunk.toString();
+        });
+        request.on('end', () => {
+            callback(querystring.decode(receivedData));
+        });
+    }
+    else {
+        callback(null);
+    }
+}
 
 //rinde		5100
 //mina		5109
+
+//see töötab
