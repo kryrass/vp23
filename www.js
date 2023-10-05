f44fd1a6e63114498b1ebb634ee7a7525298b847
Kryslin Rass <kryrass@users.noreply.github.com>
2023-10-05 14:21:31 +0300

    Kolmas kodutöö

--- www.js
+++ www.js
@@ -2,9 +2,12 @@ const http = require("http");
 const url = require("url");
 const path = require("path");
 const fs = require("fs");
+const datetimeValue = require("./datetime_et");
+const semester = require("./semesterprogress");
 
 const pageHead = '<!DOCTYPE html><html><head><meta charset="utf-8"><title>Kryslin Rass, veebiprogrammeerimine 2023</title><script src="first.js" defer></script></head><body>'
 const pageBanner = '<img src="banner.png" alt="Kursuse bänner">'
+const tluPicture='\n\t<img src="tlu_42.jpg" alt="Tallinna Ülikool">';
 const pageBody = '<h1>Kryslin Rass</h1><p>See veebileht on valminud <a href="https://www.tlu.ee" target="_blank">TLÜ</a> Digitehnoloogiate instituudi informaatika eriala õppetöö raames.</p>'
 const pageFoot = '<hr></body></html>'
 
@@ -16,7 +19,10 @@ http.createServer(function(req, res){
 	res.write(pageHead);
 	res.write(pageBanner);
 	res.write(pageBody);
+	res.write("<p>Lehe avamise aeg oli " + datetimeValue.timeOfDayET() + " ja kell on " + datetimeValue.timeNowET() + "</p><p>Kuupäev on " + datetimeValue.dateNowET() + "</p>");
 	res.write('\n\t<hr>\n\t<p><a href ="addname">Lisa oma nimi</a>!</p>');
+	res.write('<p><a href="semesterprogress">Semestri kulg</a></p>');
+	res.write('<p><a href="picture">Tore pilt.</a></p>');
 	res.write(pageFoot);
 	//console.log("keegi vaatab");
 	return res.end();
@@ -31,6 +37,44 @@ else if (currentURL.pathname == "/addname"){
 	res.write('\n\t<p>Edaspidi lisame siia asju<p>');
 	res.write(pageFoot);
 }
+else if (currentURL.pathname==="/semesterprogress")
+	{
+		res.writeHead(200, {"Content-Type":"text/html"});
+		res.write(pageHead);
+		res.write(pageBanner);
+		res.write(pageBody);
+		res.write("<p>" + semester.result + "</p>");
+		res.write("<meter min='0' value='" + semester.semesterLastedFor + "' max='" + semester.semesterDuration + "'></meter>")
+		res.write(pageFoot);
+		return res.end();
+	}
+else if (currentURL.pathname==="/picture")
+	{
+		res.writeHead(200, {"Content-Type":"text/html"});
+		res.write(pageHead);
+		res.write(pageBanner);
+		res.write(pageBody);
+		res.write(tluPicture);
+		res.write(pageFoot);
+		return res.end();
+	}
+else if (currentURL.pathname==="/tlu_42.jpg")
+	{
+		console.log("Tahan pilti!");
+		let filePath=path.join(__dirname, "public", "tluphotos/tlu_42.jpg");
+		fs.readFile(filePath, (err, data)=>
+		{
+			if(err)
+			{
+				throw err;
+			}
+			else
+			{
+				res.writeHead(200, {"Content-Type":"image/png"});
+				res.end(data);
+			}
+		});
+	}
 
 else if (currentURL.pathname === "/banner.png"){
 	console.log("Tahame pilti!");
@@ -47,9 +91,8 @@ else if (currentURL.pathname === "/banner.png"){
 	});
 }
 else {
-	console.log("Tuli ära!");
-	res.writeHead(200, {"Content-type": "image/png"});
-	res.end(data);
+	
+	res.end("error 404");
 }
 }).listen(5109);
 
